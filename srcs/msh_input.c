912642cfb57a7199c598a07c691676b86d9d5232
Benson Lee <blee@e1z2r4p40.42.us.org>
2019-02-25 18:55:37 -0800

    small update

--- srcs/msh_input.c
+++ srcs/msh_input.c
@@ -6,34 +6,43 @@
 /*   By: blee <blee@student.42.us.org>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/23 15:29:12 by blee              #+#    #+#             */
-/*   Updated: 2019/02/22 18:54:48 by blee             ###   ########.fr       */
+/*   Updated: 2019/02/25 18:40:27 by blee             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int		cmd_id(char *str, int max)
+int		cmd_id(char *str)
 {
 	int		id;
 	int		i;
 
 	id = 0;
-	i = 0;
-	while (str[i])
-	{
+	i = -1;
+	while (str[++i])
 		id += str[i];
-		i++;
-	}
-	if (max == 0)
-		return (0);
-	return (id % max);
+	if (id == 223)
+		return (0);//ls
+	else if (id == 199)
+		return (1);//cd
+	else if (id == 415)
+		return (2);//echo
+	else if (id == 329)
+		return (3);//env
+	else if (id == 661)
+		return (4);//setenv
+	else if (id == 888)
+		return (5);//unsetenv
+	else if (id == 442)
+		return (6);//exit
+	return (-1);
 }
 
 int		msh_input(char **av)
 {
 	int		id;
 
-	id = cmd_id(av[0], 7);
+	id = cmd_id(av[0]);
 	ft_printf("%d\n", id);
 	return (id);
 }
