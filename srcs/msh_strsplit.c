824d3e8d65bb5e56972c99b21f303b4934a4f9a2
Benson Lee <blee@e1z2r5p13.42.us.org>
2019-04-17 17:22:19 -0700

    strsplit works now

--- srcs/msh_strsplit.c
+++ srcs/msh_strsplit.c
@@ -6,7 +6,7 @@
 /*   By: blee <blee@student.42.us.org>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/03 18:43:25 by blee              #+#    #+#             */
-/*   Updated: 2019/04/15 16:29:02 by blee             ###   ########.fr       */
+/*   Updated: 2019/04/17 17:21:06 by blee             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,19 @@
 
 //string split that also handles quotation marks and split by spaces/tabs/etc
 
-int		cpy_to_char(char **dest, char *src, int st, char c)
+int		cpy_to_c(char *dst, char *src, int *i, int *st, char c)
 {
-	char	*temp;
-	int		i;
+	int		size;
 
-	temp = *dest;
-	i = st;
-	while(*src || *src != c)
+	size = *i;
+	*st = *st + 1;
+	while(src[*st] && src[*st] != c)
 	{
-		temp[i] = *src;
-		src++;
-		i++;
+		dst[*i] = src[*st];
+		*st = *st + 1;
+		*i = *i + 1;
 	}
-	return (i - st);
+	return (*i - size);
 }
 
 char	*dup_substr(char *str, int st, int end)
@@ -41,19 +40,18 @@ char	*dup_substr(char *str, int st, int end)
 	i = 0;
 	jump = 0;
 	new = ft_strnew(size);
-	while (st <=  end)
+	while (st < end)
 	{
 		if (str[st] == '\\' && (str[st + 1] == '\"' || str[st + 1] == '\''))
-				st++;
-		else if (str[st] == '\"' || str[st] == '\'')
+			st++;
+		if (str[st] == '\'' || str[st] == '\"')
+			cpy_to_c(new, str, &i, &st, str[st]);
+		else
 		{
-			jump = cpy_to_char(&new, &str[st], i, str[st]);
-			st += jump;
-			i += jump;
+			new[i] = str[st];
+			i++;
 		}
-		new[i] = str[st];
 		st++;
-		i++;
 	}
 	return (new);
 }
@@ -107,10 +105,10 @@ int		substr_len(char *str)
 		if (str[i] == '\\' && (str[i + 1] == '\'' || str[i + 1] == '\"'))
 			i++;
 		else if (str[i] == '\'' || str[i] == '\"')
-			i += (skip_to_c(&str[i], str[i]) - 1);
+			i += skip_to_c(&str[i], str[i]);
 		i++;
 	}
-	return (i - 1);
+	return (i);
 }
 
 int		split_count(char *str)
@@ -141,11 +139,13 @@ char	**msh_strsplit(char *str)
 {
 	int		i;
 	int		si;
+	int		substr;
 	int		splits;
 	char	**out;
 
 	i = 0;
 	si = 0;
+	substr = 0;
 	splits = split_count(str);
 	ft_printf("Found %d inputs\n", splits);
 	out = (char**)malloc(sizeof(char*) * (splits + 1));
@@ -154,7 +154,10 @@ char	**msh_strsplit(char *str)
 	{
 		while(str[si] <= ' ')
 			si++;
-		out[i] = dup_substr(str, si, substr_len(&str[si]));
+		substr = substr_len(&str[si]);
+		ft_printf("Copying length: %d\n", substr);
+		out[i] = dup_substr(str, si, substr + si);
+		si += substr;
 		i++;
 	}
 	return (out);
