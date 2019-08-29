5eadec36ca31013a664eafbcfcf4940c28b116bb
Benson Lee <blee@e1z3r5p23.42.us.org>
2019-08-28 18:15:20 -0700

    small edits

--- srcs/msh_new_cmd.c
+++ srcs/msh_new_cmd.c
@@ -6,25 +6,25 @@
 /*   By: blee <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/26 17:10:25 by blee              #+#    #+#             */
-/*   Updated: 2019/08/26 18:12:45 by blee             ###   ########.fr       */
+/*   Updated: 2019/08/28 17:04:22 by blee             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-char        *msh_flags(char **av)
+char        **cpy_args(char **av)
 {
-    char    *flags;
     int     i;
+    char    **args;
 
-    flags = NULL;
+    i = 0;
+    while (av[i])
+        i++;
+    args = (char**)malloc(sizeof(char*) * i);
     i = 1;
     while (av[i])
-    {
-        if (av[i][0] == '-')
 
-        i++;
-    }
+
 }
 
 t_cmds      *msh_new_cmd(char **av)
@@ -34,4 +34,5 @@ t_cmds      *msh_new_cmd(char **av)
     cmd = (t_cmds*)malloc(sizeof(t_cmds));
     cmd->cmd_id = msh_cmd_id(av);
     cmd->name = ft_strdup(av[0]);
+
 }
